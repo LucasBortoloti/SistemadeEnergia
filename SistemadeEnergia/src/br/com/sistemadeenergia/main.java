3c88a29cc401e59d7b249fea8ee28a7226a49c55
aluno <aluno@172.16.3.21>
2022-06-22 17:28:16 -0300

    Criação do Menu

--- SistemadeEnergia/src/br/com/sistemadeenergia/main.java
+++ SistemadeEnergia/src/br/com/sistemadeenergia/main.java
@@ -3,8 +3,33 @@ package br.com.sistemadeenergia;
 public class main {
 
 	public static void main(String[] args) {
+		Tarifa t = new Tarifa();
 		
-		System.out.println("Ola mundo");
+		
+		System.out.println("1 - CADASTRAR CLIENTE");
+		
+		System.out.println("2 - CADASTRAR UNIDADES CONSUMIDORAS");
+		
+		System.out.println("3 - FAZER LEITURA");
+		
+		System.out.println("3.1 - Informar a leitura atual");
+		
+		System.out.println("4 - FATURAR CONSUMO");
+
+		System.out.println("5 - EXIBIR RELATÓRIOS");
+
+		System.out.println("5.1 - Todos os clientes");
+		
+		System.out.println("5.2 - Faturar pendentes");
+		      
+		System.out.println("5.3 - Faturas de um cliente específico (cpf/cnpj)");
+		   
+		System.out.println("5.4 - Todas as faturas de um mês em específico (informado pelo usuário)");
+		 
+
+		    
+		
+		//System.out.println("Ola mundo: "+ t.getTarResidencial1());
 	}
 
 }
