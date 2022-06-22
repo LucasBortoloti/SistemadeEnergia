12bdb79fb7fed3ec682561a85918e486d828e2ab
aluno <aluno@172.16.3.21>
2022-06-22 17:30:55 -0300

    Metodos get na tarifa

--- SistemadeEnergia/src/br/com/sistemadeenergia/Tarifa.java
+++ SistemadeEnergia/src/br/com/sistemadeenergia/Tarifa.java
@@ -2,7 +2,7 @@ package br.com.sistemadeenergia;
 
 public class Tarifa {
 	
-	private double tarResidencial1 = 0.38790;
+	private  double tarResidencial1 = 0.38790;
 	
 	private double tarResidencial2 = 0.40500;
 	
@@ -13,5 +13,31 @@ public class Tarifa {
 	private double icms = 27;
 	
 	private double taxaIluminacaoPublica = 7.8;
+
+	public double getTarResidencial1() {
+		return tarResidencial1;
+	}
+
+	public double getTarResidencial2() {
+		return tarResidencial2;
+	}
+
+	public double getTarComercial1() {
+		return tarComercial1;
+	}
+
+	public double getTarComercial2() {
+		return tarComercial2;
+	}
+
+	public double getIcms() {
+		return icms;
+	}
+
+	public double getTaxaIluminacaoPublica() {
+		return taxaIluminacaoPublica;
+	}
+	
+	
 
 }
